d9262f93b54b11faf70f617a676fc2a125d77ef9
iradbouzidi <irad.bouzidi@esprit.tn>
2023-07-25 17:08:39 +0100

    vercel deploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useRef } from "react";
-import * as tf from "@tensorflow/tfjs";
 import * as facemesh from "@tensorflow-models/facemesh";
 import Webcam from "react-webcam";
 import "./App.css";
